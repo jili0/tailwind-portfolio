c14f4f30e79d5439d03c4436f7910d45c22a7857
Jing <jingli0319@gmail.com>
2025-04-06 22:01:51 +0200

    add project images und datdata

---
+++ projects.js
@@ -0,0 +1,114 @@
+export const projects = [
+  {
+    name: "Music App",
+    projectLink: "https://jingli.work/music-app",
+    repoLink: "https://github.com/jili0/music-app",
+    description: "A web-based music player that allows users to listen to their favorite tracks. Includes features like playlists and song search function. Team-project developed together with Dursel Türkan and Mame Angelique Guisse.",
+    imgSrc: "/projectImages/music_app.png"
+  },
+  {
+    name: "Markdown Previewer",
+    projectLink: "https://markdown-previewer-a9ax.onrender.com",
+    repoLink: "https://github.com/jili0/markdown-previewer",
+    description: "A live markdown previewer that instantly shows your markdown syntax in real-time. Built with React and powered by marked.js for markdown parsing.",
+    imgSrc: "/projectImages/markdown_previewer.png"
+  },
+  {
+    name: "Amazing Shop App",
+    projectLink: "https://amazing-shop.onrender.com",
+    repoLink: "https://github.com/jili0/amazing-shop",
+    description: "An e-commerce platform with a beautiful UI/UX. Includes product listings and add to cart function.",
+    imgSrc: "/projectImages/amazing_shop_app.png"
+  },
+  {
+    name: "Pawpose",
+    projectLink: "https://pawpose.onrender.com",
+    repoLink: "https://github.com/jili0/pawpose",
+    description: "PawPals – Finde das perfekte Zuhause für Hunde!",
+    imgSrc: "/projectImages/pawpose.png"
+  },
+  {
+    name: "My Projects",
+    projectLink: "https://jingli.work/",
+    repoLink: "https://github.com/jili0/jili0.github.io",
+    description: "A central hub for all my projects. This website serves as a gateway where you can easily explore all of my works, from utilities to full-scale applications, and find links to each of them.",
+    imgSrc: "/projectImages/my_projects.png"
+  },
+  {
+    name: "My GitHub Profile",
+    projectLink: "https://jingli.work/jili0",
+    repoLink: "https://github.com/jili0/jili0",
+    description: "My GitHub profile where I share my journey as an aspiring Full Stack Developer. You can explore my projects, including my work with React, Node.js, MongoDB, and other web technologies. I’m eager to connect with fellow developers and collaborate on new challenges.",
+    imgSrc: "/projectImages/my_github_profile.png"
+  },
+  {
+    name: "Roman Numeral Converter",
+    projectLink: "https://jingli.work/roman-numeral-converter",
+    repoLink: "https://github.com/jili0/roman-numeral-converter",
+    description: "A simple utility to convert roman numerals to integers and vice versa.",
+    imgSrc: "/projectImages/roman_numeral_converter.png"
+  },
+  {
+    name: "Search Pokemon",
+    projectLink: "https://jingli.work/search-pokemon",
+    repoLink: "https://github.com/jili0/search-pokemon",
+    description: "A fun app that lets you search for any Pokémon using the public Pokémon API.",
+    imgSrc: "/projectImages/search_pokemon.png"
+  },
+  {
+    name: "User Page",
+    projectLink: "https://user-page.onrender.com",
+    repoLink: "https://github.com/jili0/user-page",
+    description: "A user profile page that shows user information.",
+    imgSrc: "/projectImages/user_page.png"
+  },
+  {
+    name: "Change Calculator",
+    projectLink: "https://jingli.work/change-calculator",
+    repoLink: "https://github.com/jili0/change-calculator",
+    description: "A simple calculator that helps you determine the change you need after a purchase.",
+    imgSrc: "/projectImages/change_calculator.png"
+  },
+  {
+    name: "To Do App",
+    projectLink: "https://jingli.work/to-do-app",
+    repoLink: "https://github.com/jili0/to-do-app",
+    description: "A minimalistic to-do list app that allows users to add, edit, and delete tasks, stored in the browser’s local storage.",
+    imgSrc: "/projectImages/to_do_app.png"
+  },
+  {
+    name: "Quote Machine",
+    projectLink: "https://quote-machine-hqqt.onrender.com",
+    repoLink: "https://github.com/jili0/quote-machine",
+    description: "A simple app that generates random quotes from an API. Great for inspiration.",
+    imgSrc: "/projectImages/quote_machine.png"
+  },
+  {
+    name: "Python Weather",
+    projectLink: "https://python-weather-h7fr.onrender.com",
+    repoLink: "https://github.com/jili0/python-weather",
+    description: "A weather app built with Python, which pulls weather data from a third-party API.",
+    imgSrc: "/projectImages/python_weather.png"
+  },
+  {
+    name: "Mood Pad",
+    projectLink: "https://jingli.work/mood-pad",
+    repoLink: "https://github.com/jili0/mood-pad",
+    description: "An app to track and reflect on your mood over time and integrates authentication, with a beautiful UI and Firebase backend.",
+    imgSrc: "/projectImages/mood_pad.png"
+  },
+  {
+    name: "Portfolio",
+    projectLink: "https://portfolio-fcdn.onrender.com",
+    repoLink: "https://github.com/jili0/portfolio",
+    description: "A personal portfolio website where I showcase my skills, projects, and achievements. It’s designed to give visitors an easy way to explore my work and get in touch with me.",
+    imgSrc: "/projectImages/portfolio.png"
+  },
+  {
+    name: "Tailwind Portfolio",
+    projectLink: "https://tailwind-portfolio-koi2.onrender.com",
+    repoLink: "https://github.com/jili0/tailwind-portfolio",
+    description: "Another personal portfolio website that I built, using Tailwind CSS for design.",
+    imgSrc: "/projectImages/tailwind_portfolio.png"
+  }
+];
