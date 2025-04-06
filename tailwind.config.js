659c14adb9cbb72a0c68b1c5d25bb75635a53d37
Jing <jingli0319@gmail.com>
2025-04-06 22:28:16 +0200

    update path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,css,js}"],
+  content: ["./src/**/*.{html,css,js}"],
   theme: {
     extend: {
       animation: {
